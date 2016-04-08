998f251a6e49d96d5fdbb5f90768caea52f7f07e
Nabilhas1 <xnabilhasanx@gmail.com>
2016-04-08 01:49:58 -0400

    still havent completed while loop

--- Lit.cpp
+++ Lit.cpp
@@ -3,10 +3,36 @@
 
 using namespace std;
 
+// read page 519 of c++ book
 
 int main(){
 
   ofstream dataFile("gameHistory.txt");
-  dataFile.close();
+
+  string champName;
+
+  string lane;
+
+  int kill;
+
+  int death;
+
+  int assist;
+
+  int creepScore;
+
+  string result;
+
+  string notes;
+
+  if (dataFile.is_open()) {
+
+    cout << "Please enter champion name, lane, kill, death, assist, cs, result, notes: " << endl;
+    cout << "Once you are done enter done to end." << endl;
+
+    while ( cin
+
+
+  
 
 }
