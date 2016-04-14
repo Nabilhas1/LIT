eb4d498bc7c77bd16fc548ff7bd763ce342c6386
Nabilhas1 <xnabilhasanx@gmail.com>
2016-04-13 23:54:47 -0400

    added Game class header file.

---
+++ Game.h
@@ -0,0 +1,59 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+
+ private:
+
+  string m_champ;
+  string m_lane;
+  int m_kill;
+  int m_death;
+  int m_assist;
+  int m_creepScore;
+  string result;
+  string notes;
+
+
+  int kda;
+
+
+
+ public:
+
+  Game();
+  Game();
+  Game();
+
+  void setChamp(string champName);
+  string getChamp();
+
+  void setLane(string lane);
+  string getLane();
+
+  void setKill(int kill);
+  int getKill();
+
+  void setDeath(int death);
+  int getDeath();
+
+  void setAssist(int assist);
+  int getAssist();
+
+  void setCS(int creepScore);
+  int getCS();
+
+  void setResult(string result);
+  string getResult();
+
+  void setNotes(string notes);
+  string getNotes();
+
+  void setKDA(double KDA);
+  double getKDA();
+
+
+
+};
+
+#endif
